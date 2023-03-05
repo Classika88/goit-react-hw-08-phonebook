74b5857a20dbcb84ea43797968390f98659444e8
Iryna Khamidova <waterholein@gmail.com>
2023-03-05 16:53:44 +0200

    ++

--- src/redux/auth/authOperation.js
+++ src/redux/auth/authOperation.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
+import { getTimeMeasureUtils } from '@reduxjs/toolkit/dist/utils';
 
 axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
 
